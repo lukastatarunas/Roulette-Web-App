8a9f500c652f0cd8d72fdb2dff63c4db7459cd17
lukas.tatarunas@shawk.eu <lukas.tatarunas@shawk.eu>
2019-12-06 02:07:44 +0200

    Updates

---
+++ precache-manifest.48bf700d06a9e6233c8cab00a5772dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce62be0cd9cd7b3386334d38a7b22db8",
+    "url": "/Roulette-Web-App/index.html"
+  },
+  {
+    "revision": "45c2ef3ab8df7ea1d3ef",
+    "url": "/Roulette-Web-App/static/css/main.c4f25f8b.chunk.css"
+  },
+  {
+    "revision": "1fe9a47b0b76347c72ba",
+    "url": "/Roulette-Web-App/static/js/2.a67e9833.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Roulette-Web-App/static/js/2.a67e9833.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45c2ef3ab8df7ea1d3ef",
+    "url": "/Roulette-Web-App/static/js/main.8c1dafbf.chunk.js"
+  },
+  {
+    "revision": "fa6b99a2917c90d355fb",
+    "url": "/Roulette-Web-App/static/js/runtime-main.0be5ce29.js"
+  }
+]);
